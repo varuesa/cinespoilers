371cb43d096afbcad125cc6ecc398c063728254e
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-29 21:21:19 -0500

    feat: Update MovieGrid Component

--- src/ui/components/modules/MovieGrid.tsx
+++ src/ui/components/modules/MovieGrid.tsx
@@ -1,40 +1,41 @@
 import type { Movie } from '../../../shared/types/movie.types';
 import MovieCard from '../components/MovieCard';
 
-export interface MovieGridProps {
+interface MovieGridProps {
+  id: string;
+  title: string;
   movies: Movie[];
+  favorites?: Movie[];
+  onToggleFavorite?: (movie: Movie) => void;
 }
 
-const MovieGrid = ({ movies }: MovieGridProps) => {
-  return (
-    <section id="now-showing" className="section">
-      <div className="container d-flex f-direction-column g-8">
-        <div className="d-flex f-direction-column a-items-center g-2">
-          <h2 className="title c-primary t-align-center">
-            Now Showing 🎬
-          </h2>
-          <p className="text text--lg c-secondary-text t-align-center">
-            Discover the latest movies without spoilers - see everything you need to know!
-          </p>
-        </div>
+const MovieGrid = ({
+  id,
+  title,
+  movies,
+  favorites = [],
+  onToggleFavorite
+}: MovieGridProps) => (
+  <section className="section" id={id}>
+    <div className="container d-flex f-direction-column g-8">
+      <h2 className="title c-primary t-align-center">{title}</h2>
 
+      {movies.length === 0 ? (
+        <p className="text text--lg t-align-center">No movies found 😞</p>
+      ) : (
         <div className="g-layout g-layout--auto-fit-columns g-8">
-          {movies.length > 0 ? (
-            movies.map(movie => <MovieCard key={movie.id} movie={movie} />)
-          ) : (
-            <div className="d-flex f-direction-column a-items-center g-4">
-              <p className="text c-secondary-text">
-                🎭 No movies available right now
-              </p>
-              <p className="text c-secondary-text">
-                Check back soon for the latest releases!
-              </p>
-            </div>
-          )}
+          {movies.map(movie => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              isFavorite={favorites.some(favorite => favorite.id === movie.id)}
+              onToggleFavorite={onToggleFavorite}
+            />
+          ))}
         </div>
-      </div>
-    </section>
-  );
-};
+      )}
+    </div>
+  </section>
+);
 
 export default MovieGrid;
